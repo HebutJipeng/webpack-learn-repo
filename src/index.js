edafce2f546947566e67d6a8f39c490e2c6bd487
jipeng <jipeng@gotokeep.com>
2019-12-23 18:45:52 +0800

    lazyload

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
-import { cube } from './math'
+import _ from "lodash";
 
-if (process.env.NODE_ENV !== 'production') {
-    console.log('Looks like we are in development mode!');
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
 }
 
-function getComponent() {
-    return import(/* webpackChunkName: "lodash" */ 'lodash').then(({ default: _ }) => {
-        let element = document.createElement('pre') 
-        element.innerHTML = _.join(['hello webpack!', '5 cubed is equal to' + cube(5)], ' ')
-        return element
-    }).catch(error => 'An error occurred while loading the component');
+function component() {
+  var element = document.createElement("div");
+  var button = document.createElement("button");
+  const br = document.createElement("br");
+
+  button.innerHTML = "Click me and look at the console!";
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.appendChild(br);
+  element.appendChild(button);
+
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.onclick = e =>
+    import(/* webpackChunkName: "print" */ "./print").then(module => {
+      var print = module.default;
+
+      print();
+    });
+
+  return element;
 }
 
-
-getComponent().hasAttributeNS(component => {
-    document.body.appendChild(component)
-})
+document.body.appendChild(component());
