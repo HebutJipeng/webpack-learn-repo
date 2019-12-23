0a24eaaf0bb3323ed50b8f90fa740418ff097834
jipeng <jipeng@gotokeep.com>
2019-12-23 16:27:02 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import _ from 'lodash'
-import './style.css'
-import Icon from './picture.png'
+import printMe from './print'
 
 function component() {
    let element = document.createElement('div') 
+   let btn = document.createElement('button')
 
    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
    element.classList.add('hello')
 
-   const myIcon = new Image()
-   myIcon.src = Icon
-   element.appendChild(myIcon)
+   btn.innerHTML = 'click & console'
+   btn.onclick = printMe
+
+   element.appendChild(btn)
 
    return element
 }
