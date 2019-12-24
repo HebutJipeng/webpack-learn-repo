b5ed2857f68766e78c574aed9b62dcd00f1f9dec
HebutJipeng <282602893@qq.com>
2019-12-24 11:06:09 +0800

    lib

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
-import _ from "lodash";
-// import Print from './print'
+import _ from 'lodash';
+import numRef from './ref.json';
 
-if (process.env.NODE_ENV !== "production") {
-  console.log("Looks like we are in development mode!");
+export function numToWord(num) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.num === num ? ref.word : accum;
+  }, '');
 }
 
-function component() {
-  var element = document.createElement("div");
-  var button = document.createElement("button");
-  const br = document.createElement("br");
-
-  button.innerHTML = "Click me and look at the console!";
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.appendChild(br);
-  element.appendChild(button);
-
-  // Note that because a network request is involved, some indication
-  // of loading would need to be shown in a production-level site/app.
-//   element.click = Print.bind(null, 'hello wp')
-
-  return element;
+export function wordToNum(word) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
 }
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,18 @@
 const path = require('path')
-const HtmlWebpackPlugin= require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const WebpackManifestPlugin = require('webpack-manifest-plugin')
-const webpack = require('webpack')
-
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/index.js',
-    },
+    entry: './src/index.js',
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'webpack-numbers.js',
+      library: 'webpackNumbers',
+      libraryTarget: 'umd'
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: '管理输出'
-        }),
-        new WebpackManifestPlugin(),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    optimization: {
-        usedExports: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: ['file-loader']
-            }
-        ]
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
     }
-}
+};
